fabce9191ce11662a432f47f40e710d2fbbce6b2
yoshwa <ryoshwaa@gmail.com>
2025-02-27 15:04:30 +0530

    Added Router with /favorite ;

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css';
 import './components/MovieCard';
+import NavBar from './components/NavBar';
 import Favorite from './pages/Favorite';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+  <div>
+    <NavBar />
     <main className="main-content">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path= "/favorite" element = {<Favorite/>} />
       </Routes>
     </main>
+    </div>
   );
 } export default App;
